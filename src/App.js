6e3fd959f3c8737a7e189d2dac611be9e0f749cd
Fukatsummy <fukatsummy@gmail.com>
2023-10-11 20:33:11 +0300

    create botton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Button from './Components/Button/Button';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hi i am ShopBot</h1>
+    <div>
+      <h1>Hi i am shopoBot</h1>
+      <Button></Button>
     </div>
   );
 }
+
 export default App;
