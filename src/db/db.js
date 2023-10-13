b9dc6aa1ad210f3ae44ff243ceb03c51e1979910
Fukatsummy <fukatsummy@gmail.com>
2023-10-13 20:23:46 +0300

    Create Menu

--- src/db/db.js
+++ src/db/db.js
@@ -7,8 +7,8 @@ import saladImg from "../images/salad.png"
 import waterImg from "../images/water.png"
 import colaImg from "../images/coca.png"
 
-function getData(){
-    return(
+export function getData(){
+    return[
         {title:"Pizza", price:17.99, Image: pizzaImg, id:1},
         {title:"Burger", price:10.99, Image: burgerImg, id:2},
         {title:"IceCream", price:8.99, Image: icecreamImg, id:3},
@@ -16,5 +16,6 @@ function getData(){
         {title:"Kebab", price:7.99, Image: kebabImg, id:5},
         {title:"Salad", price:9.99, Image: saladImg, id:6},
         {title:"Water", price:5.99, Image: waterImg, id:7}, 
-        {title:"ola", price:6.99, Image: colaImg, id:8}   );
+        {title:"Cola", price:6.99, Image: colaImg, id:8}   ];
 }
+//export default getData
